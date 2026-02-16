1627d10d1d649997d26c717ad7745ae53149cdee
Mahesh More <maheshhiralalmore321@gmail.com>
2026-02-16 10:43:10 +0530

    created pages and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { SubmitEnquiry } from './pages/submit-enquiry/submit-enquiry';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    },
+    {
+        path:'home',
+        component:Home
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'submit-enquiry',
+        component:SubmitEnquiry
+    }
+];

7af09b706a25e678edab596b36e349f2c76302a9
Mahesh More <maheshhiralalmore321@gmail.com>
2026-02-18 14:03:12 +0530

    submit enuiry form is ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { SubmitEnquiry } from './pages/submit-enquiry/submit-enquiry';
+import { EnquiryList } from './pages/enquiry-list/enquiry-list';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path:'submit-enquiry',
         component:SubmitEnquiry
+    },
+    {
+        path:'enquiry-list',
+        component:EnquiryList
     }
 ];
--- src/app/pages/submit-enquiry/submit-enquiry.ts
+++ src/app/pages/submit-enquiry/submit-enquiry.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master-service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-submit-enquiry',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './submit-enquiry.html',
   styleUrl: './submit-enquiry.css',
 })
-export class SubmitEnquiry {
+export class SubmitEnquiry implements OnInit {
+
+  masterService = inject(MasterService);
+  categoryList: any[] = [];
+  statusList: any[] = [];
+
+  newEnquiryObj: any = {
+  "enquiryId": 0,
+  "customerName": "",
+  "customerEmail": "",
+  "customerPhone": "",
+  "message": "",
+  "categoryId": 0,
+  "statusId": 0,
+  "enquiryType": "",
+  "isConverted": false,
+  "enquiryDate": new Date(),
+  "followUpDate": "",
+  "feedback": ""
+}
+
+
+  ngOnInit(): void {
+    this.getCategory();
+    this.getStatus();
+
+  }
+
+  getCategory() {
+    this.masterService.getAllCategory().subscribe({
+      next: (result: any) => {
+        this.categoryList = result.data;
+        console.log('list',this.categoryList)
+      }
+    })
+  }
+
+  getStatus() {
+    this.masterService.getAllStatus().subscribe({
+      next: (result: any) => {
+        this.statusList = result.data;
+      }
+    })
+  }
+
+  onSaveEnquiry(){
+    this.masterService.saveNewEnquiry(this.newEnquiryObj).subscribe({
+      next:(result:any)=>{
+        alert("Enquiry submitted successfully")
+      },
+      error:(error:any)=>{
+        alert('Error from submit enquiry api')
+      }
+    })
+  }
 
 }

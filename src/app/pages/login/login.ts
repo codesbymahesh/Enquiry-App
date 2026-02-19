4d8934a9df31a453a21fc2ccb84a9f738cbe886b
Mahesh More <maheshhiralalmore321@gmail.com>
2026-02-19 23:05:54 +0530

    static login page

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  loginObj: any = {
+    username: "",
+    pwd: ""
+  }
+  router = inject(Router)
+
+  onLogin() {
+    if (this.loginObj.username === 'admin' && this.loginObj.pwd === '223344') {
+      alert('login successful');
+      localStorage.setItem('enuiryApp', 'adminLoggedIn')
+      this.router.navigateByUrl('/enquiry-list')
+    }
+    else {
+      alert('wrong credentials');
+    }
+  }
 
 }
